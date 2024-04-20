18ddfc539953c519a744b050d9711a898f6b7090
Benard <benardkhajira@gmail.com>
2024-04-20 09:34:21 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const init = () => {
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Movie not found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const title = document.querySelector("section#movieDetails h4");
+      const summary = document.querySelector("section#movieDetails p");
+  
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    })
+    .catch((error) => {
+      console.error("Error fetching movie data:", error);
+    });
   
 }
 
